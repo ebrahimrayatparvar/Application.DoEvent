7ea9f6601b658931e98634c1623c4f75561f0271
Ebrahimrayatparvar <ebrahim.rayatparvar@hotmail.com>
2019-12-14 17:22:04 +0330

    init

--- DoEvent/Form1.cs
+++ DoEvent/Form1.cs
@@ -16,5 +16,16 @@ namespace DoEvent
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 30000; i++)
+            {
+                textBox1.Text = i.ToString();
+                Application.DoEvents();
+                i += 10;
+                textBox1.Text = i.ToString();
+            }
+        }
     }
 }
